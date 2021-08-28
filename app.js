7aef04dfc38a65c4eda57f2e90dd479c5deb9280
Joshua Moore <J_moore23@hotmail.com>
2021-08-28 10:55:02 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,136 @@
+let resource = 0
+let Str = 0
+let Focus = 0
+let Item2 = 0
+let Pass2 = 0
+let sPrice = 20
+let dPrice = 50
+let buff1 = 100
+let buff2 = 200
+let sMulti = 0
+let dMulti = 0
+let hpBuff = 0
+let xpBuff = 0
+
+let Clickupgrades = {
+  str: {
+    quantity: 0,
+    multiplier: 0,
+    price: 20
+  },
+  item: {
+    quantity: 0,
+    multiplier: 0,
+    price: 0,
+  }
+  }
+let autoUpgrades = {
+  focus: {
+    quantity: 0,
+    multiplier: 0,
+    price: 0
+  },
+  pass: {
+    quantity: 0,
+    multiplier: 0,
+    price: 0,
+  }
+}
+
+
+function mine(){
+  resource++
+  for(let key in Clickupgrades){
+  resource +=  Clickupgrades[key].multiplier * Clickupgrades[key].quantity
+  console.log(resource)
+  document.getElementById('resourceCount').innerText = Math.round(resource).toString()
+  }
+}
+
+function strBuy(key){
+  if(resource >= sPrice){
+    resource -= sPrice
+    sPrice += 20
+    Str += 1
+    Clickupgrades[key].quantity += 1
+    Clickupgrades[key].multiplier += .5
+    sMulti += Clickupgrades[key].multiplier * Clickupgrades[key].quantity
+  } else   
+  window.alert('you broke foo')
+  document.getElementById('strCount').innerText = Str.toString()
+  document.getElementById('sPrice').innerText = sPrice.toString()
+  document.getElementById('sMulti').innerText = sMulti.toString()
+  update()
+}
+
+function ItemTwo(key){
+  if(resource >= 50){
+    resource -= dPrice
+    dPrice += 50
+    Item2 ++
+    Clickupgrades[key].quantity += 1
+    Clickupgrades[key].multiplier += 1.25
+    dMulti += Clickupgrades[key].multiplier * Clickupgrades[key].quantity
+  }else 
+  window.alert("nah son")
+  document.getElementById('itTwo').innerText = Item2.toString()
+  document.getElementById('dPrice').innerText = dPrice.toString()
+  document.getElementById('dMulti').innerText = dMulti.toString()
+  update()
+  
+}
+function focusUp(key){
+  if(resource >= 100){
+    resource -= buff1
+    buff1 += 100
+    Focus += 1
+    autoUpgrades[key].quantity += 1
+    autoUpgrades[key].multiplier += 5
+    hpBuff += autoUpgrades[key].multiplier * autoUpgrades[key].quantity
+ } else   
+ window.alert('you broke foo')
+   document.getElementById('focusCount').innerText = Focus.toString()
+   document.getElementById('buff1').innerText = buff1.toString()
+   document.getElementById('hpBuff').innerText = hpBuff.toString()
+   update()
+  
+}
+function passUp(key){
+  if(resource >= 200){
+    resource -= buff2
+    buff2 += 200
+    Pass2 += 1
+    autoUpgrades[key].quantity += 1
+    autoUpgrades[key].multiplier += 10
+    xpBuff += autoUpgrades[key].multiplier * autoUpgrades[key].quantity
+ } else   
+ window.alert('you broke foo')
+   document.getElementById('passCount').innerText = Pass2.toString()
+   document.getElementById('buff2').innerText = buff2.toString()
+   document.getElementById('xpBuff').innerText = xpBuff.toString()
+  update()
+}
+   
+   
+
+
+function autoInterval(collectionInterval){
+  collectionInterval = setInterval(collectAutoUpgrades, 3000);
+}
+autoInterval()
+
+
+function collectAutoUpgrades(){
+  for(let key in autoUpgrades){
+resource += autoUpgrades[key].multiplier * autoUpgrades[key].quantity
+console.log(resource)
+document.getElementById('resourceCount').innerText = Math.round(resource).toString()
+  }
+}
+
+
+function update(){
+document.getElementById('resourceCount').innerText =resource.toString()
+}
+
+
